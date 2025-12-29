21be3cc0e0d40158e6a2d9cf311b47343f62b89c
Nashita Sattar <nashita.sattar@yahoo.com>
2025-12-29 17:21:51 -0500

    Initial Set Up

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import {TaskList} from "./pages/TaskList"
+import {Header} from "./component/Header"
+import {Footer} from "./component/Footer"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        {<Header/>}
+        <Routes>
+          <Route path="/" element={<TaskList/>} />
+        </Routes>
+        {<Footer/>}
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
